fef0d2b3d875f8a5c2708d03102573433cafd1ff
jdwv <sandpit@jdwv.me>
2022-12-04 18:33:13 +1100

    Add testing flag to template

--- 2022/template/answer.py
+++ 2022/template/answer.py
@@ -3,11 +3,17 @@
 import os
 DirName = os.path.dirname(__file__)
 FileName = os.path.join(DirName, 'input.txt')
+Testing = False
 
-with open(FileName, "r") as File:
-    Data = [line.strip() for line in File]
-
-Test = """1
+TestData = """1
 2
 3
 4""".split("\n")
+
+if Testing:
+    Data = TestData    
+else:
+    with open(FileName, "r") as File:
+        Data = [line.strip() for line in File]
+
+print(Data)
